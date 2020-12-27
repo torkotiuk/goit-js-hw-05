6172480590b53d58e842522c3bcd6e47495b0b45
Andrii Torkotiuk <torkotiuk@gmail.com>
2020-12-27 17:42:45 +0100

    hw-5

---
+++ js/task-5.js
@@ -0,0 +1,68 @@
+class Car {
+  static getSpecs(car) {
+    console.log(car);
+  }
+  
+  constructor({speed, price, maxSpeed, isOn, distance}) {
+    this.speed = speed;
+    this._price = price;
+    this.maxSpeed = maxSpeed;
+    this.isOn = isOn;
+    this.distance = distance;
+  }
+
+  get price() {
+    return this._price;
+  }
+
+  set price(newPrice) {
+    this._price = newPrice;
+  }
+
+  turnOn() {
+    this.isOn = true;
+  }
+
+  turnOff() {
+    this.isOn = false;
+  }
+
+  accelerate(value) {
+    this.speed = value;
+  }
+
+  decelerate(value) {
+    if (this.speed > 0) {
+      this.speed -= value;
+    } 
+  }
+
+  drive(hours) {
+    let distance = 0;
+    this.distance = distance;
+    if (this.isOn) {
+      this.distance = hours * this.speed;
+    }
+  }
+}
+
+const mustang = new Car({ maxSpeed: 200, price: 2000 });
+
+mustang.turnOn();
+mustang.accelerate(50);
+mustang.drive(2);
+
+Car.getSpecs(mustang);
+// maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+
+mustang.decelerate(20);
+// mustang.drive(4.3333333333333334);
+mustang.drive(1);
+mustang.turnOff();
+
+Car.getSpecs(mustang);
+// maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+
+console.log(mustang.price); // 2000
+mustang.price = 4000;
+console.log(mustang.price); // 4000
